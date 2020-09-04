ce67bf63e053f24467b3a6974e96f7c7535af927
Kiran Kamble <70654607+kiranwithcode@users.noreply.github.com>
2020-09-04 22:35:06 +0530

    Assingment day 3

---
+++ Assignment Day3.py
@@ -0,0 +1,30 @@
+# Question 1
+# You all are Pilots, you want to land a plane safely, so altitude required for landing a plane is
+# 1000ft, it it is less than tell pilot to land the plane, or it is more than that but less than 5000ft ask
+# the pilot to “come down to 1000ft”, else if it more than 5000ft ask the pilot to “go around and try
+# later”
+# num=int(input("Enter Number:"))
+# if num<1000:
+#     print("Land The Plane")
+# elif num==1000:
+#     print("Safe To Land")
+# elif num==4500:
+#     print("Bring Down To 1000")
+# elif num==6500:
+#     print("Turn Around")
+# else:
+#     pass
+
+# Question 2
+# Using for loop please print all the prime numbers between 1- 200 using FOR LOOP AND RANGE
+# function.
+num=1
+l=[]
+for i in range(num,201):
+    if i%2!=0:
+        l.append(i)
+print(l)
+    
+        
+
+    
